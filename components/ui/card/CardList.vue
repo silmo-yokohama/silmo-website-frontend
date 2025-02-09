07056f0c47dc2b39542029f67f0c0f487a3e0d33
silmo-yokohama <silmo.yokohama@gmail.com>
2025-02-09 22:44:08 +0900

    実績・ポートフォリオ掲載ページ実装

---
+++ components/ui/card/CardList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    class?: string;
+  }>();
+
+  const mergedClasses = mergeClassNames(props.class || '', 'grid grid-cols-1 md:grid-cols-2 gap-8');
+</script>
+
+<template>
+  <div :class="mergedClasses">
+    <slot />
+  </div>
+</template>
